ba1089386ed547068db787350b76b43f9ab92e45
Xhweri <102481213+Xhweri@users.noreply.github.com>
2023-05-24 23:07:00 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Labb3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sparaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sparaSomToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.öppnaNyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.radera = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 31);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(749, 662);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menyToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(773, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menyToolStripMenuItem
+            // 
+            this.menyToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sparaToolStripMenuItem,
+            this.sparaSomToolStripMenuItem,
+            this.öppnaNyToolStripMenuItem,
+            this.radera});
+            this.menyToolStripMenuItem.Name = "menyToolStripMenuItem";
+            this.menyToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.menyToolStripMenuItem.Text = "Meny";
+            this.menyToolStripMenuItem.Click += new System.EventHandler(this.menyToolStripMenuItem_Click);
+            // 
+            // sparaToolStripMenuItem
+            // 
+            this.sparaToolStripMenuItem.Name = "sparaToolStripMenuItem";
+            this.sparaToolStripMenuItem.Size = new System.Drawing.Size(162, 26);
+            this.sparaToolStripMenuItem.Text = "Spara";
+            this.sparaToolStripMenuItem.Click += new System.EventHandler(this.sparaToolStripMenuItem_Click);
+            // 
+            // sparaSomToolStripMenuItem
+            // 
+            this.sparaSomToolStripMenuItem.Name = "sparaSomToolStripMenuItem";
+            this.sparaSomToolStripMenuItem.Size = new System.Drawing.Size(162, 26);
+            this.sparaSomToolStripMenuItem.Text = "Spara som";
+            this.sparaSomToolStripMenuItem.Click += new System.EventHandler(this.sparaSomToolStripMenuItem_Click);
+            // 
+            // öppnaNyToolStripMenuItem
+            // 
+            this.öppnaNyToolStripMenuItem.Name = "öppnaNyToolStripMenuItem";
+            this.öppnaNyToolStripMenuItem.Size = new System.Drawing.Size(162, 26);
+            this.öppnaNyToolStripMenuItem.Text = "Öppna ny";
+            this.öppnaNyToolStripMenuItem.Click += new System.EventHandler(this.öppnaNyToolStripMenuItem_Click);
+            // 
+            // radera
+            // 
+            this.radera.Name = "radera";
+            this.radera.Size = new System.Drawing.Size(162, 26);
+            this.radera.Text = "Radera";
+            this.radera.Click += new System.EventHandler(this.radera_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(773, 705);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "namnlös.txt";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sparaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem öppnaNyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem radera;
+        private System.Windows.Forms.ToolStripMenuItem sparaSomToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Labb3
+{
+    public partial class Form1 : Form
+    {
+        bool osparad = false;   
+        string filepath = string.Empty;
+        bool abortMisson=false;
+        bool sparaClicked = false;
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void menyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radera_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void sparaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                if (!File.Exists(filepath))
+                {
+                    SaveFileDialog saveFileDialog1 = new SaveFileDialog(); //Skapar Spara rutan i windows.
+                    saveFileDialog1.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";//Egenskap av Savefile som filtrerar de slags filer användaren kan se i rutan.         
+                    saveFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);//Den standard Pathen för windows operativsystem till dokument,Rutan börjar där.
+
+                    if (saveFileDialog1.ShowDialog() == DialogResult.OK)//Kollar om användaren tryckte på okej eller avbryt i rutan. ok tryckt händer följande..
+                    {
+                        using (StreamWriter sw = new StreamWriter(saveFileDialog1.FileName))
+                        {
+                            filepath = saveFileDialog1.FileName;
+                            sw.Write(textBox1.Text);
+                            ActiveForm.Text = Path.GetFileName(filepath);
+                            sparaClicked = true;
+                        }
+                    }
+                }
+                ActiveForm.Text = Path.GetFileName(filepath);
+
+
+            }
+            catch
+            {
+                MessageBox.Show("Filen kunde ej sparas!");
+                
+            }
+        }
+
+        private void sparaSomToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog(); //Skapar Spara rutan i windows.
+            saveFileDialog1.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";//Egenskap av Savefile som filtrerar de slags filer användaren kan se i rutan.         
+            saveFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);//Den standard Pathen för windows operativsystem till dokument,Rutan börjar där.
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)//Kollar om användaren tryckte på okej eller avbryt i rutan. ok tryckt händer följande..
+            {
+                using (StreamWriter sw = new StreamWriter(saveFileDialog1.FileName))
+                {
+                    filepath = saveFileDialog1.FileName;
+                    sw.Write(textBox1.Text);
+                    ActiveForm.Text = Path.GetFileName(filepath);
+                    sparaClicked = true;
+                }
+            }
+        }
+
+
+        private void öppnaNyToolStripMenuItem_Click(object sender, EventArgs e)
+        {            try
+            {                abortMisson = false;
+                if (ActiveForm.Text.Contains("*"))
+                {
+var unSaved = MessageBox.Show("Du har inte sparat din fil vill du fortsätta?",
+                        "Spara",
+                        MessageBoxButtons.YesNoCancel,
+                        MessageBoxIcon.Warning
+                        );
+            switch(unSaved)
+                    {
+                        case DialogResult.Yes:
+                            abortMisson = false;
+                            break;
+                        case DialogResult.No:
+                            abortMisson = true;
+
+                            SaveFileDialog saveFileDialog1 = new SaveFileDialog(); //Skapar Spara rutan i windows.
+                            saveFileDialog1.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";//Egenskap av Savefile som filtrerar de slags filer användaren kan se i rutan.         
+                            saveFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);//Den standard Pathen för windows operativsystem till dokument,Rutan börjar där.
+
+                            if (saveFileDialog1.ShowDialog() == DialogResult.OK)//Kollar om användaren tryckte på okej eller avbryt i rutan. ok tryckt händer följande..
+                            {
+                                using (StreamWriter sw = new StreamWriter(saveFileDialog1.FileName))
+                                {
+                                    sw.Write(textBox1.Text);
+                                    osparad = false;
+                                }
+                            }
+
+                            break;
+                            case DialogResult.Cancel:
+                            return;
+                            break;
+
+
+                    }
+                }
+                
+                if (abortMisson == false)
+                {
+                    OpenFileDialog opna = new OpenFileDialog();
+                    if (opna.ShowDialog() == DialogResult.OK && abortMisson == false)
+                    {
+                        filepath = opna.FileName;
+                        textBox1.Text = File.ReadAllText(filepath);
+                        ActiveForm.Text = Path.GetFileName(filepath);
+                        osparad= false;
+                    }
+                }
+
+
+
+
+            }
+            catch
+            {
+                MessageBox.Show("Programmet fick ett avbrott!");
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (sparaClicked)
+            {
+                ActiveForm.Text = Path.GetFileName(filepath);
+                sparaClicked= false;
+            }
+            
+            if (File.Exists(filepath))
+            {
+                ActiveForm.Text = "* " + Path.GetFileName(filepath);
+                osparad = true;
+            }
+            else
+            {
+                string nyForm = "* ";
+                ActiveForm.Text = nyForm + "namnlös.txt";
+            }
+
+        }
+
+        
+    }
+}
